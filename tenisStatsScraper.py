c7843d9741209a0a17c5b4f6a5853b8aa50e4ee5
Laertes <marcoslaertes@outlook.com>
2019-02-23 02:12:57 +0100

    Added check for existing file

--- tenisStatsScraper.py
+++ tenisStatsScraper.py
@@ -17,38 +17,47 @@ from selenium.webdriver.firefox.options import Options
 import csv
 import os
 
-def scrape(player):
+def statsExists(player):
     f = "./Tenis_Stats.csv"
-    if os.path.exists(f):
+    if os.path.exists(f) and len(stats) == 0:
         df = pd.read_csv("Tenis_Stats.csv")
-    options = Options()
-    options.headless = True # Esto es para que no se habra la ventana de firefox
-    profile = webdriver.FirefoxProfile()
-    # 1 - Allow all images
-    # 2 - Block all images
-    # 3 - Block 3rd party images 
-    profile.set_preference("permissions.default.image", 2) # Esto es para que no se carguen las imagenes
-    browser = webdriver.Firefox(firefox_profile=profile, options=options)
-    browser.get("https://www.ultimatetennisstatistics.com/")
-    search = browser.find_element(By.XPATH, '//*[@id="player"]')
-    search.send_keys(player)
-    time.sleep(2)
-    search.send_keys(Keys.ARROW_DOWN)
-    search.send_keys(Keys.RETURN)
-    time.sleep(3)
-    browser.find_element(By.XPATH,"/html/body/ul[1]/li[9]/a").click()
-    time.sleep(2)
-    browser.find_element(By.XPATH,'//*[@id="statisticsPill"]').click()
-    time.sleep(2)
-    soup = BeautifulSoup(browser.page_source, 'html.parser')
-    browser.close()
-    stats = []
-    serve_html = soup.find_all('table', class_= 'table table-condensed table-hover table-striped')
-    for serve in serve_html:
-        data = serve.find_all('th', {"class": ["text-right pct-data", "text-right"]})
-        for i in data:
-            i = i.get_text()
-            if i != '' and '/' not in i:
-                pal = i.replace('%','').replace('\n','')
-                stats.extend([pal])
-    return(stats)
+        stats = df.loc[df['player'] == player]
+        if stats.empty:
+            return []
+        else:
+            return stats
+def scrape(player):
+    stats = statsExists(player)
+    if stats != []:
+        return stats
+    else:
+        options = Options()
+        options.headless = True # Esto es para que no se habra la ventana de firefox
+        profile = webdriver.FirefoxProfile()
+        # 1 - Allow all images
+        # 2 - Block all images
+        # 3 - Block 3rd party images 
+        profile.set_preference("permissions.default.image", 2) # Esto es para que no se carguen las imagenes
+        browser = webdriver.Firefox(firefox_profile=profile, options=options)
+        browser.get("https://www.ultimatetennisstatistics.com/")
+        search = browser.find_element(By.XPATH, '//*[@id="player"]')
+        search.send_keys(player)
+        time.sleep(2)
+        search.send_keys(Keys.ARROW_DOWN)
+        search.send_keys(Keys.RETURN)
+        time.sleep(3)
+        browser.find_element(By.XPATH,"/html/body/ul[1]/li[9]/a").click()
+        time.sleep(2)
+        browser.find_element(By.XPATH,'//*[@id="statisticsPill"]').click()
+        time.sleep(2)
+        soup = BeautifulSoup(browser.page_source, 'html.parser')
+        browser.close()
+        serve_html = soup.find_all('table', class_= 'table table-condensed table-hover table-striped')
+        for serve in serve_html:
+            data = serve.find_all('th', {"class": ["text-right pct-data", "text-right"]})
+            for i in data:
+                i = i.get_text()
+                if i != '' and '/' not in i:
+                    pal = i.replace('%','').replace('\n','')
+                    stats.extend([pal])
+        return(stats)
