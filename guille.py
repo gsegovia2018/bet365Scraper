364c600de429c115a7ed39f7e247d690ee164ca3
Guillermo Segovia <GSegovia@CMC>
2021-11-29 13:18:38 -0500

    First commit and requirements

---
+++ guille.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import time
+import sys
+import os
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
+from tenisStatsScraper import scrape
+
+scrape("Tommy Paul")
